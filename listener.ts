8496661ba4e5d5b79597deff016d499758a5546a
Dmytro Kravchuk <dimakravchuck93@gmail.com>
2022-09-26 15:27:03 +0300

    init

--- listener.ts
+++ listener.ts
@@ -2,23 +2,34 @@ const fs = require("fs")
 require('dotenv').config();
 const WEB3 = require("web3")
 const bigNumber = require("@ethersproject/bignumber")
-const CONTROLLER = require("./ERC20.json")
+const { ethers } = require("ethers");
+const CONTROLLER = require("./abi/Controller.json")
+const tokenUSDT = require("./abi/TokenUSDT.json")
+const tokenPP = require("./abi/TokenPP.json")
+
+const TOKEN =  process.env.TOKEN
+const CONTROLLER_ADDRESS = process.env.CONTROLLER
+const ADDRESS_OWNER = process.env.ADDRESS_OWNER
+const TOKEN_PP_ADDRESS = process.env.CONTRACT_ADR_TOKEN_PP
+const TOKEN_USDT_ADDRESS = process.env.CONTRACT_ADR_TOKEN_USDT
 
 const listener = () => {
-    const provider =  process.env.PROVIDER
-    const token =  process.env.TOKEN
-    const controllerAddress = process.env.CONTROLLER
+    const provider = new ethers.providers.JsonRpcProvider(process.env.PROVIDER);
+    const signer = provider.getSigner()
+    const getContract = async () => {
+        const contractController = await new ethers.Contract(CONTROLLER_ADDRESS, CONTROLLER.abi, provider)
+        const contractPP = await new ethers.Contract(TOKEN_PP_ADDRESS, tokenPP.abi, provider)
+        const contractUSDT = await new ethers.Contract(TOKEN_USDT_ADDRESS, tokenUSDT.abi, provider)
 
-    const we3Provider = new WEB3(provider)
-    we3Provider.eth.setProvider(provider)
-
-   const contractController = new we3Provider.eth.Contract([CONTROLLER], token)
-
-    const getBitsRoles = async () => {
-        return await contractController.methods.decimals.call()
+        return {
+            contractController,
+            contractPP,
+            contractUSDT
+        }
     }
 
-    console.log(getBitsRoles())
+    const {contractController, contractPP, contractUSDT } = getContract()
+    console.log(getContract().then(data => console.log(data)))
 }
 
 module.exports = listener;
