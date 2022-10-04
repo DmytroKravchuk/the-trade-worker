ffbca0690b0843966f37d151a073ba3e512eec82
Nikolaj <kolambo4ka@gmail.com>
2022-10-04 21:19:30 +0300

    hueta

--- changeTick.ts
+++ changeTick.ts
@@ -1,35 +1,50 @@
-import {BigNumber} from "ethers";
+import { BigNumber } from 'ethers';
 const ethers = require('ethers');
-import SwapRouter from "./abi/SwapRouter.json"
+import SwapRouter from './abi/SwapRouter.json';
 
-const ROUTER_ADDRESS = process.env.ROUTER_ADDRESS || ""
+const ROUTER_ADDRESS = process.env.ROUTER_ADDRESS || '';
 
-const changeTick = async ({token0, token1, amount, provider, wallet, poolContract, int24, int24_2}: any) => {
-    const contractController = await new ethers.Contract(ROUTER_ADDRESS, SwapRouter.abi, provider)
-    const executionFee: BigNumber = ethers.utils.parseEther("0.003");
-    let currentTick = (await poolContract.slot0()).tick;
-    console.log(`int24 ${int24}`)
-    console.log(`int24_2 ${int24_2}`)
-    while(!(currentTick >= int24 && currentTick <= int24_2)) {
-        currentTick = (await poolContract.slot0()).tick;
-        console.log(`Current tick: ${currentTick}`)
-        const swap = await contractController.connect(wallet).exactInputSingle({
-            tokenIn: token1,
-            tokenOut: token0,
-            fee: 3000,
-            recipient: wallet.address,
-            deadline: 1e10,
-            amountIn: amount,
-            amountOutMinimum: 1,
-            sqrtPriceLimitX96: 0
-        }, {
-            value: executionFee
-        })
-        await swap.wait();
-        currentTick = (await poolContract.slot0()).tick;
-        console.log(`Current tick end: ${currentTick}`)
-    }
-}
+const changeTick = async ({
+  token0,
+  token1,
+  amount,
+  provider,
+  wallet,
+  poolContract,
+  int24,
+  int24_2,
+}: any) => {
+  const contractController = await new ethers.Contract(
+    ROUTER_ADDRESS,
+    SwapRouter.abi,
+    provider,
+  );
+  const executionFee: BigNumber = ethers.utils.parseEther('0.003');
+  let currentTick = (await poolContract.slot0()).tick;
+  console.log(`int24 ${int24}`);
+  console.log(`int24_2 ${int24_2}`);
+  while (!(currentTick >= int24 && currentTick <= int24_2)) {
+    currentTick = (await poolContract.slot0()).tick;
+    console.log(`Current tick: ${currentTick}`);
+    const swap = await contractController.connect(wallet).exactInputSingle(
+      {
+        tokenIn: token0,
+        tokenOut: token1,
+        fee: 3000,
+        recipient: wallet.address,
+        deadline: 1e10,
+        amountIn: amount,
+        amountOutMinimum: 1,
+        sqrtPriceLimitX96: 0,
+      },
+      {
+        value: executionFee,
+      },
+    );
+    await swap.wait();
+    currentTick = (await poolContract.slot0()).tick;
+    console.log(`Current tick end: ${currentTick}`);
+  }
+};
 
-module.exports = changeTick
-
+module.exports = changeTick;
--- listeners/listener-ethers.ts
+++ listeners/listener-ethers.ts
@@ -1,83 +1,76 @@
 // @ts-ignore
-import {BigNumber, utils} from "ethers";
+import { BigNumber, utils } from 'ethers';
 const ethers = require('ethers');
 require('dotenv').config();
 
-const generateCurrentTick = require("../generateCurrentTick")
-import CONTROLLER from "../abi/Controller.json"
-const changeTick = require('../changeTick')
-const approveToken = require('../approveToken')
+const generateCurrentTick = require('../generateCurrentTick');
+import CONTROLLER from '../abi/Controller.json';
+const changeTick = require('../changeTick');
+const approveToken = require('../approveToken');
 // @ts-ignore
-const providerHandler = require("../providers/test-provider-ethers")
+const providerHandler = require('../providers/test-provider-ethers');
 
-const CONTROLLER_ADDRESS = process.env.CONTROLLER
-const ROUTER_ADDRESS = process.env.ROUTER_ADDRESS
+const CONTROLLER_ADDRESS = process.env.CONTROLLER;
+const ROUTER_ADDRESS = process.env.ROUTER_ADDRESS;
 
 // @ts-ignore
 const ListenerEthers = async ({ gasPrice }) => {
-    const {wallet, provider} = providerHandler()
-    const getContract = async () => {
-        const contractController = await new ethers.Contract(CONTROLLER_ADDRESS, CONTROLLER.abi, provider)
-        return {
-            contractController
-        }
-    }
+  const { wallet, provider } = providerHandler();
+  const getContract = async () => {
+    const contractController = await new ethers.Contract(
+      CONTROLLER_ADDRESS,
+      CONTROLLER.abi,
+      provider,
+    );
+    return {
+      contractController,
+    };
+  };
+  const filter = {
+    address: CONTROLLER_ADDRESS,
+    topics: [
+      utils.id(`CreateOrder(address,address,uint256,int24,int24,uint8)`),
+    ],
+  };
+  const { contractController } = await getContract();
 
-    const filter = {
-        address: CONTROLLER_ADDRESS,
-        topics: [
-            utils.id(`CreateOrder(address,address,uint256,int24,int24,uint8)`
-            ),
-        ]
-    }
-    const {contractController} = await getContract()
+  const sendHandler = async ({ address2, uint256 }: any) => {
+    const executeTRX = await contractController
+      .connect(wallet)
+      .executeOrder(uint256, address2)
+      .then(() => {
+        console.log('Order Executed');
+        console.log(executeTRX);
+      });
 
-    const sendHandler = async ({address2, uint256}: any) => {
-        await contractController.connect(wallet).executeOrder(uint256, address2).then(() => {
-            console.log('Order Executed')
-        })
+    // await contractController.connect(wallet).cancelOrder(uint256, address2).then(() => {
+    //     console.log('Cancel Order')
+    // })
+  };
+  //////
+  // "desiredTick":
+  //     This is desired tick to achive.
+  //      In case to close order desired tick whould be "tickUpper" from orders() function for order type 0
+  //       (Buy Limit), and "tickLower" for order type 1 (Take Profit).
+  //надо кароче через if/else подбирать тик и свапать так же, главное починить цикл на апруве и свапе
 
-        // await contractController.connect(wallet).cancelOrder(uint256, address2).then(() => {
-        //     console.log('Cancel Order')
-        // })
-    }
-
-    contractController.on(filter, (address1: any, address2: any, uint256: any, int24: any, int24_2: any, OrderType: any) => {
-        const amount = BigNumber.from("100000000000000000000000")
-        console.log("int24")
-        console.log(int24)
-        console.log(int24_2)
-        generateCurrentTick({poolAddress: address2, provider}).then(({token0, token1, slot0, poolContract}: any) => {
-            approveToken({
-                address: token0,
-                address2: ROUTER_ADDRESS,
-                provider, wallet,
-                amount: amount
-            }).then(async () => {
-               await approveToken({
-                    address: token1,
-                    address2: ROUTER_ADDRESS,
-                    provider, wallet,
-                    amount: amount
-                })
-                changeTick({
-                    token0,
-                    token1,
-                    amount: amount,
-                    desiredTick: slot0.tick,
-                    provider,
-                    wallet,
-                    poolContract,
-                    int24,
-                    int24_2
-                }).then(() => {
-                   console.log("sendHandler")
-                   sendHandler({address2, uint256})
-                })
-            })
-        })
-
-    })
-}
+  contractController.on(filter,(address1: any,address2: any,uint256: any,int24: any,int24_2: any,OrderType: any,) => {
+      const amount = BigNumber.from('80000000000000000000000');
+      console.log('int24');
+      console.log(int24);
+      console.log(int24_2);
+      generateCurrentTick({ poolAddress: address2, provider }).then(async ({token0, token1, slot0, poolContract }: any) => {
+        await approveToken({address: token0,address2: ROUTER_ADDRESS,provider,wallet,amount: amount,}).then(async () => {
+        await approveToken({address: token1,address2: ROUTER_ADDRESS,provider,wallet,amount: amount,});
+        changeTick({token0,token1,amount: amount,desiredTick: slot0.tick,provider,wallet,poolContract,int24,int24_2,}).then(() => {
+        console.log('sendHandler');
+        sendHandler({ address2, uint256 });
+        });
+        });
+        },
+      );
+    },
+  );
+};
 
 module.exports = ListenerEthers;
